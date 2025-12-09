ca5567d8a50993ad0f378f60819f67a01d1e3922
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2025-12-09 20:30:39 +0300

    add cracker and start js

---
+++ Trap-Guessing-Game/js/app.js
@@ -0,0 +1,24 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const crakers =[]
+
+/*-------------------------------- Variables --------------------------------*/
+
+let phase = 1 // three phases 1 to player one pick bombs place,  2 for second player, 3 to strat
+let turn = 1 // two trun 1 for player one , 2 player 2
+
+
+// to store the bombs index 
+
+let p1Bomb= []
+let p2Bomb= []
+
+// the number of hearts each player has
+
+let p1Hearts = 3 
+let p2Hearts = 3 
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
