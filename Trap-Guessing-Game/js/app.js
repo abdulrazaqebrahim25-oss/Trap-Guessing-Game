ab48a56b83c992437fc352dc4314859ed5c96b6a
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2025-12-12 18:28:24 +0300

    Update the js with vraiables

--- Trap-Guessing-Game/js/app.js
+++ Trap-Guessing-Game/js/app.js
@@ -1,24 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const crakers =[]
-
 /*-------------------------------- Variables --------------------------------*/
 
-let phase = 1 // three phases 1 to player one pick bombs place,  2 for second player, 3 to strat
-let turn = 1 // two trun 1 for player one , 2 player 2
+// 1 = P1 places bombs, 2 = P2 places bombs, 3 = battle
+let phase = 1;
+let turn = 1;
 
+// store bomb locations
+let p1Bomb = [];
+let p2Bomb = [];
 
-// to store the bombs index 
+// hearts
+let p1Hearts = 3;
+let p2Hearts = 3;
 
-let p1Bomb= []
-let p2Bomb= []
-
-// the number of hearts each player has
-
-let p1Hearts = 3 
-let p2Hearts = 3 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
+
