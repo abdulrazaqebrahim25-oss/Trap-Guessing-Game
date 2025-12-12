43687cfe8e717e232fd1f87ede9fa394aff372dd
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2025-12-12 18:31:05 +0300

    add cached elementr references

--- Trap-Guessing-Game/js/app.js
+++ Trap-Guessing-Game/js/app.js
@@ -16,6 +16,17 @@ let p2Hearts = 3;
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squares = document.querySelectorAll(".sqr");
+const startBtn = document.querySelector(".strat");
+
+const p1Heart1 = document.getElementById("p1-hearts-1");
+const p1Heart2 = document.getElementById("p1-hearts-2");
+const p1Heart3 = document.getElementById("p1-hearts-3");
+
+const p2Heart1 = document.getElementById("p2-hearts-1");
+const p2Heart2 = document.getElementById("p2-hearts-2");
+const p2Heart3 = document.getElementById("p2-hearts-3");
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
